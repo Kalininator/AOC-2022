c095bd452e2fe7537f11111c61b426cb6f208d14
Alexander Kalinin <alex@kalinin.uk>
2022-12-01 09:14:09 +0000

    cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,26 +1,21 @@
 fn main() {
     let lines = utils::read_arg_file_lines();
 
-    let split_sections: Vec<Vec<usize>> = lines
+    let calories_by_elf: Vec<Vec<usize>> = lines
         .split(|l| l.is_empty())
-        .map(|section| {
-            section
-                .iter()
-                .map(|v| v.parse::<usize>().unwrap())
-                .collect()
-        })
+        .map(|section| section.iter().map(|v| v.parse().unwrap()).collect())
         .collect();
 
-    let mut summed_sections: Vec<usize> = split_sections
+    let mut elf_total_calories: Vec<usize> = calories_by_elf
         .iter()
         .map(|section| section.iter().sum())
-        .collect::<Vec<usize>>();
-    summed_sections.sort();
-    summed_sections.reverse();
+        .collect();
+    elf_total_calories.sort();
+    elf_total_calories.reverse();
 
-    let most_calories = summed_sections[0];
+    let most_calories = elf_total_calories[0];
     println!("Elf carrying most calories is carrying {most_calories} calories.");
 
-    let top_3_elf_total_calories: usize = summed_sections.iter().take(3).sum();
+    let top_3_elf_total_calories: usize = elf_total_calories.iter().take(3).sum();
     println!("Top 3 elves are carrying a total of {top_3_elf_total_calories} calories.");
 }
