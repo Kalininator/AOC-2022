8b72f98770a747a7e4b6b4f1b93afed61c18ea89
Alexander Kalinin <alex@kalinin.uk>
2022-12-01 09:26:40 +0000

    remove unused utils

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -8,18 +8,3 @@ pub fn read_arg_file_lines() -> Vec<String> {
 
     br.lines().map(|l| l.expect("Failed to get line")).collect()
 }
-
-pub fn read_full_file(path: &str) -> String {
-    fs::read_to_string(path).expect("Failed to read file")
-}
-
-pub fn binary_to_decimal(bools: Vec<bool>) -> u32 {
-    let mut acc: u32 = 0;
-    for i in bools {
-        match i {
-            true => acc = (acc * 2) + 1,
-            false => acc *= 2,
-        }
-    }
-    acc
-}
