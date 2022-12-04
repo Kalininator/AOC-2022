c94bd82d233506d3d45bbb3ce59fc49fcea3982e
Alexander Kalinin <alex@kalinin.uk>
2022-12-04 12:39:38 +0000

    refactor day04 to sscanf

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,20 +1,18 @@
-#[macro_use]
-extern crate scan_fmt;
+use sscanf::sscanf;
 
 fn main() {
     let unparsed_elf_pairs = utils::read_arg_file_lines();
     let mut complete_overlaps: usize = 0;
     let mut partial_overlaps: usize = 0;
     for line in unparsed_elf_pairs {
-        if let Ok((a, b, c, d)) = scan_fmt!(&line, "{d}-{d},{d}-{d}", usize, usize, usize, usize) {
-            if (a <= c && b >= d) || (c <= a && d >= b) {
-                complete_overlaps += 1;
-                partial_overlaps += 1;
-                continue;
-            }
-            if (b >= c && a <= c) || (a <= d && b >= d) {
-                partial_overlaps += 1;
-            }
+        let (a, b, c, d) = sscanf!(&line, "{u8}-{u8},{u8}-{u8}").unwrap();
+        if (a <= c && b >= d) || (c <= a && d >= b) {
+            complete_overlaps += 1;
+            partial_overlaps += 1;
+            continue;
+        }
+        if (b >= c && a <= c) || (a <= d && b >= d) {
+            partial_overlaps += 1;
         }
     }
     println!("Completely overlapping elf pairs: {complete_overlaps}");
