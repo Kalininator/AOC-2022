16b92ac2989dabfa945599654b4b5fb3e2777f4b
Alexander Kalinin <alex@kalinin.uk>
2022-12-06 13:13:32 +0000

    add tests

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -15,3 +15,67 @@ fn main() {
     println!("Part 1: {:?}", marker_location(line, 4));
     println!("Part 2: {:?}", marker_location(line, 14));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_example_1() {
+        assert_eq!(marker_location("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), Some(5));
+    }
+    #[test]
+    fn part_1_example_2() {
+        assert_eq!(marker_location("nppdvjthqldpwncqszvftbrmjlhg", 4), Some(6));
+    }
+    #[test]
+    fn part_1_example_3() {
+        assert_eq!(
+            marker_location("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4),
+            Some(10)
+        );
+    }
+    #[test]
+    fn part_1_example_4() {
+        assert_eq!(
+            marker_location("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4),
+            Some(11)
+        );
+    }
+
+    #[test]
+    fn part_2_example_1() {
+        assert_eq!(
+            marker_location("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14),
+            Some(19)
+        );
+    }
+    #[test]
+    fn part_2_example_2() {
+        assert_eq!(
+            marker_location("bvwbjplbgvbhsrlpgdmjqwftvncz", 14),
+            Some(23)
+        );
+    }
+    #[test]
+    fn part_2_example_3() {
+        assert_eq!(
+            marker_location("nppdvjthqldpwncqszvftbrmjlhg", 14),
+            Some(23)
+        );
+    }
+    #[test]
+    fn part_2_example_4() {
+        assert_eq!(
+            marker_location("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14),
+            Some(29)
+        );
+    }
+    #[test]
+    fn part_2_example_5() {
+        assert_eq!(
+            marker_location("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14),
+            Some(26)
+        );
+    }
+}
